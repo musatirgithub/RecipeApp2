b48e211e94e24eed86ee0bf845cf1bbff64cb169
Mehmet Ugur SATIR <musatir@gmail.com>
2022-07-15 11:44:04 +0300

    update

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,9 +1,26 @@
+import { MainContainer, Card, Header, Image, Button } from './Cards.style';
+import defaultImage from '../../assets/default-image.jpg';
+import { useNavigate } from 'react-router-dom';
 
-
-const Cards = () => {
+const Cards = ({ recipes }) => {
+  const navigate = useNavigate();
   return (
-    <div>Cards</div>
-  )
-}
+    <MainContainer wrap="wrap">
+      {recipes.map(({ recipe }, index) => (
+        <Card key={index}>
+          <Header>{recipe.label}</Header>
+          <Image src={recipe.image || defaultImage} />
+          <Button
+            onClick={() =>
+              navigate('detail', { state: recipe, replace: false })
+            }
+          >
+            View More
+          </Button>
+        </Card>
+      ))}
+    </MainContainer>
+  );
+};
 
 export default Cards;
--- src/components/globalStyles/Global.styles.jsx
+++ src/components/globalStyles/Global.styles.jsx
@@ -1,12 +1,14 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-*{
-    margin: 0;
+  *{
     box-sizing: border-box;
-    font-family: 'Girassol', cursive;
-}
-body{
+    margin:0;
+    font-family: 'Girassol',cursive;
+  }
+  body{
     font-size: 1.5rem;
-}
-`
+    background: ${({ theme }) => theme.colors.mainColor}
+  }
+
+`;
